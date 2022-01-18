b45264c1b0a480c5e0bcb650aff2f522b61e7d38
ImrankhanDev <imrankhantech707@gmail.com>
2022-01-18 22:24:50 +0600

    routing done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import About from "./Components/HomePage/About/About";
+import Service from "./Components/HomePage/Service/Service";
+import Features from "./Components/HomePage/Features/Features";
+import Navbar from "./Components/Shared/Navbar/Navbar";
+import Footer from "./Components/Shared/Footer/Footer";
+import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="service" element={<Service />} />
+          <Route path="features" element={<Features />} />
+          <Route path="*" element={<NotFound />} />
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <img
+        src="https://cdn.searchenginejournal.com/wp-content/uploads/2020/08/killer-404-page-dribble-5f3d622fef9b7.png"
+        alt="404 page not found"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
